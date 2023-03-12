db35e87c52390300dbe347382bf8e181f8d585da
lampd <lam@tgl-sol.com>
2023-03-12 16:32:07 +0700

    chore: test mirror

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
-  return <div className='App'>Test</div>
+  return <div className='App'>Test 1</div>
 }
 
 export default App
